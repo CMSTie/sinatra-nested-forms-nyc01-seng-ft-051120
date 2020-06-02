a4a7e6c299a67c49168c18de76542f44792799b8
CMSTie <cmstie@gmail.com>
2020-06-02 16:43:58 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+
+    attr_reader :name, :weight, :height
+    
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+    end
+
 end
